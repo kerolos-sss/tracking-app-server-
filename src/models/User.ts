94edead7e328827cb8f7c4b990db05aaabff6d8e
kerolos <kerolos.sss@gmail.com>
2020-09-15 01:20:48 +0200

    password hashing

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,17 +1,54 @@
 import mongoose, { Mongoose } from "mongoose";
-
+import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema({
     email: {
         type: mongoose.SchemaTypes.String,
         unique: true,
         required: true
-    }, 
+    },
     password: {
-        type: mongoose.SchemaTypes.String ,
+        type: mongoose.SchemaTypes.String,
         required: true
     }
 
-}) 
+})
+
+userSchema.pre("save", function (next) {
+    const user = this as { password: string } & mongoose.Document;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash
+            next()
+        })
+    })
+})
+
+
+userSchema.methods.comparePassword = function (candidatePassword: string) {
+    const user = this as Document & { password: string };
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+            if (err) {
+                return reject(err)
+            }
+            if (!isMatch) {
+                return reject(false)
+            }
+
+            resolve(true);
+        })
+    });
+}
+
 
 mongoose.model('User', userSchema)
--- src/routs/authRouter.ts
+++ src/routs/authRouter.ts
@@ -13,11 +13,35 @@ router.post('/signup', async (req, res) => {
         const { email, password } = req.body;
         const user = new User({ email, password });
 
-        const saved = await user.save()
+        const saved = await user.save();
         console.log(saved)
         let token = jwt.sign({ userId: saved._id }, MY_SECRET_KEY)
         res.send({ token });
     } catch (err) {
+        res.status(422).send(err.message)
+    }
+})
+
+router.post('/signin', async (req, res) => {
+    console.log(req.body)
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(422).send("User must provide email and password")
+    }
+
+    try {
+
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).send({ error: "Email not found. " })
+        }
+
+        await (user as any).comparePassword(password)
+
+        let token = jwt.sign({ userId: user._id }, MY_SECRET_KEY)
+        res.send({ token });
+    } catch (err) { 
         res.status(422).send(err.message)
     }
 })
